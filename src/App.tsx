cb3839a5a62724e04531d4df8bca1e94cd0526f5
Ariel Scarpinelli <ascarpinelli@spredfast.com>
2021-03-15 18:38:47 -0300

    actual initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { ChangeEvent } from 'react';
+import Score, { Part, Measure, INote } from './model/Score';
+import JSZip from 'jszip';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+type AppState = {
+  score?: Score
+  error?: any
+}
+
+class App extends React.Component<{}, AppState> {
+
+  state: Readonly<AppState> = {}
+
+  loadFile = async (event: ChangeEvent<HTMLInputElement>) => {
+    const parser = new DOMParser();
+    if (event.target.files) {
+      const file = event.target.files[0];
+      let musicXmlDom: Document;
+      try {
+        if (file.name.indexOf(".mxl") !== -1) {
+          const zip = await new JSZip().loadAsync(file);
+          const container = await zip.file("META-INF/container.xml")?.async("string");
+          const containerDoc = parser.parseFromString(container!, "text/xml");
+          const xmlPath = containerDoc.getElementsByTagName("rootfile")[0]?.getAttribute("full-path");
+          musicXmlDom = parser.parseFromString(await zip.file(xmlPath!)!.async("string"), "text/html");
+        } else {
+          musicXmlDom = parser.parseFromString(await file.text(), "text/xml")
+        }
+
+        this.setState({
+          score: Score.fromMusicXMLDom(musicXmlDom)
+        })
+      } catch (e) {
+        this.setState({
+          error: e
+        })
+      }
+    }
+  }
+
+  renderScore() {
+    if (!this.state.score) {
+      return null;
+    }
+    const score = this.state.score;
+    return (<div>
+      <h1>{score.title()}</h1>
+      {score.parts().map(part => this.renderPart(part))}
+    </div>)
+  }
+
+  renderPart(part: Part) {
+    return (<div className="part" key={part.id()}>
+      <ol className="measures">{part.measures().map(measure => this.renderMeasure(measure))}
+      </ol>
+    </div>)
+  }
+
+  renderMeasure(measure: Measure) {
+    const duration = measure.duration();
+    return (<li className="measure" value={measure.number()} key={measure.number()}>
+      {((measure.number() % 10) === 0) ? <small className={"measureNumber"}>{measure.number()}</small> : null}
+      {measure.notesAndChordsByStaff().map(staff => <ol className="stave">
+        {staff.map(note => this.renderNote(note, duration))}
+      </ol>)}
+    </li>)
+  }
+
+  renderNote(note: INote, measureDuration: number) {
+    return (<li style={{width: (note.duration() / measureDuration * 100) + '%'}}>{note.name().padEnd(3)}</li>)
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <input type="file" onChange={this.loadFile} />
+        {this.renderScore()}
+        {this.state.error ? <div>{this.state.error}</div> : null}
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/model/Score.ts
@@ -0,0 +1,234 @@
+import { notStrictEqual } from "node:assert";
+
+export class Part {
+    xml: Element;
+
+    constructor(xml:Element) {
+        this.xml = xml;
+    }
+
+    measures():Array<Measure> {
+        return Array.from(this.xml.getElementsByTagName("measure")).map(xml => new Measure(xml))
+    }
+
+    id() {
+        return this.xml.getAttribute("id")!;
+    }
+  }
+
+export class Measure {
+    xml: Element;
+
+    constructor(xml:Element) {
+        this.xml = xml;
+    }
+
+    // divisions
+    // staves
+    // cleff
+    // key
+    // time
+    // direction/sound/tempo
+
+    number() {
+        return +this.xml.getAttribute("number")!;
+    }
+
+    duration() {
+        return this.notesAndChordsByStaff().reduce((max, staff) => 
+            Math.max(staff.reduce((staffMax, note) => Math.max(note.duration(), staffMax), 0), max), 0);
+    }
+
+    notes(): Array<INote> {
+        return Array.from(this.xml.getElementsByTagName("note"))
+            .map(xml => xml.getElementsByTagName("rest").length ? new Rest(xml) : new Note(xml));
+    }
+
+    notesAndChordsByStaff(): Array<Array<INote>> {
+        // TODO: consider using "backup" elements
+        const byVoice = this.notes().reduce((acc: Map<string, INote[]>, note) => {
+            const staffIndex = note.staff() + note.voice();
+            let group = acc.get(staffIndex);
+            if (!group) {
+                group = [];
+                acc.set(staffIndex, group)
+            }
+            if (!note.inChord()) {
+                group.push(note) 
+            }  else if (note instanceof Note) {
+                const previousNote = group[group.length - 1];
+                let chord: Chord;
+                if (!(previousNote instanceof Chord)) {
+                    chord = new Chord([previousNote])
+                    group[group.length - 1] = chord;
+                } else {
+                    chord = previousNote;
+                }
+                chord.notes.push(note)
+            };
+
+            return acc;
+        }, new Map());
+
+        return Array.from(byVoice.values());
+    }
+}
+
+// In MusicXML, notes, chords, rests, etc are "notes"
+export interface INote {
+    inChord(): boolean;
+    staff(): string;
+    voice(): string;
+    name(): string;
+    duration(): number;
+} 
+
+export class Chord implements INote {
+    notes: Array<INote>;
+
+    constructor(notes:Array<INote>) {
+        this.notes = notes;
+    }
+
+    staff() {
+        return this.notes[0].staff();
+    }
+
+    voice() {
+        return this.notes[0].voice();
+    }
+
+    inChord() {
+        return false; // chords are not inside chords...
+    }
+
+    name() {
+        return this.notes.map(note => note.name()).join("\n");
+    }
+
+    duration() {
+        return this.notes.reduce((max, note) => Math.max(note.duration(), max), 0)
+    }
+
+}
+
+abstract class BaseNote implements INote {
+    xml: Element;
+
+    constructor(xml:Element) {
+        this.xml = xml;
+    }
+    
+    abstract name(): string;
+
+    staff() {
+        return this.xml.getElementsByTagName("staff")[0]?.innerHTML;
+    }
+
+    voice() {
+        return this.xml.getElementsByTagName("voice")[0]?.innerHTML;
+    }
+
+    inChord() {
+        return !!this.xml.getElementsByTagName("chord").length;
+    }
+
+    duration() {
+        return (+this.xml.getElementsByTagName("duration")[0]?.innerHTML);
+    }
+
+}
+
+interface Pitch {
+    alter: string,
+    step: string,
+    octave: number
+} 
+
+export class Note extends BaseNote {
+
+    static steps = ['C', 'D', 'E', 'F', 'G', 'A', 'B'];
+
+    alter() {
+        return 
+    }
+
+    picth():Pitch {
+        const pitch = this.xml.getElementsByTagName("pitch")[0];
+        const alterValue = pitch?.getElementsByTagName("alter")[0]?.innerHTML;
+        let octave = +pitch?.getElementsByTagName("octave")[0].innerHTML;
+        let step = pitch?.getElementsByTagName("step")[0].innerHTML;
+        let alter = "";
+
+        switch(alterValue) {
+            case '1':
+                alter = "#";
+                break;
+            case '-1':
+                alter = "b";
+                break;
+            case '2':
+                step = this.moveStep(step, 1);
+                if (step === 'C') {
+                    octave++;
+                }
+                break;
+            case '-2':
+                step = this.moveStep(step, -1);
+                if (step === 'B') {
+                    octave--;
+                }
+            }
+        
+        return {
+            alter,
+            step,
+            octave
+        };
+    }
+
+    moveStep(step: string, distance: number) {
+        return Note.steps[(Note.steps.indexOf(step)+distance) % Note.steps.length];
+    }
+
+    name() {
+        const pitch = this.picth();
+        return pitch.step + pitch.alter + pitch.octave;
+    }
+
+}
+
+export class Rest extends BaseNote {
+
+    name(): string {
+        return "-";
+    }
+
+    inChord(): boolean {
+        return false;
+    }
+
+}
+
+export default class Score {
+    xml: Document;
+
+    constructor(musicXML: Document) {
+        this.xml = musicXML;
+    }
+
+    static fromMusicXMLDom(xml: Document):Score {
+        const score = new Score(xml);
+        return score;
+    }
+
+    title() {
+        return this.xml.getElementsByTagName("work-title")[0]?.innerHTML
+    }
+
+    parts():Array<Part> {
+        // TODO - support score-timewise
+        return Array.from(this.xml.getElementsByTagName("part")).map(xml => new Part(xml))
+    }
+
+}
